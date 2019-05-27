9ad6e6ab596432275347b3b1e82a10f4bc900101
eashish93 <eashish93@gmail.com>
2019-05-28 00:03:38 +0530

    added screenshots

--- front/pages/index.js
+++ front/pages/index.js
@@ -90,9 +90,9 @@ function Index(props) {
         <Layout
             title="ImgSquash - Compress Images to smallest size"
             metaDescription="Free online image compressor for jpeg and png images with bulk compression and parallel uploading. Upto 70% reduction in original while maintaining quality.">
-            <Dropzone />
+            {/* <Dropzone /> */}
             {/* <CompressedResult /> */}
-            {/* {props.stage === 0 ? <Dropzone /> : <CompressedResult />} */}
+            {props.stage === 0 ? <Dropzone /> : <CompressedResult />}
             <HowToCompress />
             <Features />
         </Layout>
